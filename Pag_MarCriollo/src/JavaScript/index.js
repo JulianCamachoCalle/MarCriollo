3d649031fea08212e9af97da8fa6d1ca28ecbbcf
Julian Fernando Camacho Calle <u22242383@utp.edu.pe>
2024-05-14 16:10:13 -0500

    Cambios en el index para que sea responsive

--- Pag_MarCriollo/src/JavaScript/index.js
+++ Pag_MarCriollo/src/JavaScript/index.js
@@ -1,11 +1,34 @@
-//Cambiar fondo
+/*Cambiar fondo
 const mainElement = document.getElementById('main');
-const backgrounds = ['fondo1.jpg', 'fondo2.jpeg', 'fondo3.jpg'];
+const backgrounds = ['fondo1', 'fondo2', 'fondo3'];
 let currentIndex = 0;
 
 function cambiarFondo() {
-    mainElement.style.backgroundImage = `url('../img/${backgrounds[currentIndex]}')`;
+    mainElement.className = `main ${backgrounds[currentIndex]}`;
     currentIndex = (currentIndex + 1) % backgrounds.length;
 }
 
-setInterval(cambiarFondo, 5000);
+setInterval(cambiarFondo, 5000);*/
+
+//PLATOS
+const carrusel = document.querySelector('.carrusel');
+const platos = document.querySelectorAll('.plato');
+let currentIndex = 0;
+
+function mostrarPlato(index) {
+    platos.forEach(plato => {
+        plato.style.display = 'none';
+    });
+    platos[index].style.display = 'block';
+}
+
+function cambiarPlato(direccion) {
+    if (direccion === 'siguiente') {
+        currentIndex = (currentIndex + 1) % platos.length;
+    } else if (direccion === 'anterior') {
+        currentIndex = (currentIndex - 1 + platos.length) % platos.length;
+    }
+    mostrarPlato(currentIndex);
+}
+
+mostrarPlato(currentIndex);
