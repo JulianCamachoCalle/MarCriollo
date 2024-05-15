a20ba780a86847931b250c1541d9c2da944b69f7
Leoxd7i <167488937+Leoxd7i@users.noreply.github.com>
2024-05-15 11:28:44 -0500

    cambios en ccs redes sociales

--- Pag_MarCriollo/src/JavaScript/redes sociales.js
+++ Pag_MarCriollo/src/JavaScript/redes sociales.js
@@ -6,13 +6,3 @@ document.querySelectorAll('.social-link').forEach(link => {
         window.open(this.href, '_blank');
     });
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    const themeToggle = document.getElementById('theme-toggle');
-    const body = document.body;
-
-    themeToggle.addEventListener('click', function() {
-        body.classList.toggle('light-theme');
-        body.classList.toggle('dark-theme');
-    });
-});
