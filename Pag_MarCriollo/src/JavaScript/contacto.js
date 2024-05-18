f89a65e7c18cf71ad0a3a483d68df63153503859
JhordyCC <jhordycontreras08@gmail.com>
2024-05-18 00:54:48 -0500

    le agregue para que envien  mensaje

---
+++ Pag_MarCriollo/src/JavaScript/contacto.js
@@ -0,0 +1,18 @@
+const typed = new Typed('.typed', {
+    strings: [
+        '<i class="contactanoss ">ontactanos </i>', 
+        '<i class="contactanoss">ONTACTANOS </i>' 
+    ],
+    stringsElement: '#cadenas-texto',
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    smartBackspace: true,
+    shuffle: false,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '',
+    contentType: 'html',
+});
