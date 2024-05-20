d02b9f1e956cc181d5913f8aa34b91194e8deba7
Leoxd7i <167488937+Leoxd7i@users.noreply.github.com>
2024-05-20 11:51:16 -0500

    1

--- Pag_MarCriollo/src/JavaScript/index.js
+++ Pag_MarCriollo/src/JavaScript/index.js
@@ -32,3 +32,21 @@ function cambiarPlato(direccion) {
 }
 
 mostrarPlato(currentIndex); 
+
+// Función para desplazarse hacia arriba de la página
+function scrollToTop() {
+    window.scrollTo({
+        top: 0, // Desplaza hasta la parte superior de la página
+        behavior: "smooth" // Desplazamiento suave
+    });
+}
+
+// Mostrar u ocultar el botón de scroll arriba según la posición del usuario
+window.addEventListener("scroll", function() {
+    var scrollButton = document.getElementById("scrollUp"); // Obtén el botón de scroll arriba
+    if (document.documentElement.scrollTop > 20) { // Si el desplazamiento vertical es mayor a 20 pixels
+        scrollButton.style.display = "block"; // Muestra el botón de scroll arriba
+    } else {
+        scrollButton.style.display = "none"; // Oculta el botón de scroll arriba
+    }
+});
