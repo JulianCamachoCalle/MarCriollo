250a01c0e1acbb59374c57a59541c5cc68897a7e
Julian Fernando Camacho Calle <u22242383@utp.edu.pe>
2024-06-01 19:49:06 -0500

    principal

---
+++ Pag_MarCriollo/src/menuprincipal.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MarCriollo</title>
+    <link rel="stylesheet" href="style/menuprincipal.css">
+    <link rel="icon" href="img/favicon-32x32.png" type="image/png">
+    <script src="https://kit.fontawesome.com/d2b7381cec.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poetsen+One&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <div class="contenedorhead">
+            <div class="head">
+                MarCriollo
+            </div>
+            <div class="logoprincipal">
+                <img src="img/crab.png" alt="Logo">
+            </div>
+        </div>
+    </header>
+    <nav class="navbar">
+        <button id="menu-desplegable" class="menu-desplegable" aria-label="Menu Desplegable">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </button>
+        <ul class="opciones">
+            <li><a id="no-seleccionado" href="index.html">Inicio</a></li>
+            <li><a id="no-seleccionado" href="nosotros.html">Nosotros</a></li>
+            <li><a id="no-seleccionado" href="servicios.html">Servicios</a></li>
+            <li><a id="no-seleccionado" href="redessociales.html">Redes Sociales</a></li>
+            <li><a id="no-seleccionado" href="mapas.html">Mapas</a></li>
+            <li><a id="no-seleccionado" href="contacto.html">Contacto</a></li>
+            <li><a id="seleccionado" href="intranet.php">Intranet</a></li>
+        </ul>
+    </nav>
+    <script src="JavaScript/headerfooter.js"></script>
+    <main>
+
+    </main>
+    <footer>
+        <section id="redes">
+            <a href="https://www.instagram.com/">
+                <img src="img/logoig.png" alt="Instagram"></a>
+            <a href="https://twitter.com/">
+                <img src="img/logotw.png" alt="Twitter"></a>
+            <a href="https://Facebook.com/">
+                <img src="img/face.png" alt="Facebook"></a>
+        </section>
+        Jirón Salaverry 110 Magdalena del Mar Municipalidad Metropolitana de Lima LIMA, 17
+        <section id="licencias">
+            <a href="https://www.google.com/">Terminos y Condiciones</a>
+            <br>
+            <a href="https://www.google.com/">Política de Privacidad</a>
+        </section>
+        <section id="contacto">
+            <a href="tel:+51950661842">
+                <img src="img/telef.png" alt="Telefono">
+                +51 950 661 842
+            </a>
+            <a href="mailto:MarCriollo@gmail.com">
+                <img src="img/correo.png" alt="Correo">
+                MarCriollo@gmail.com
+            </a>
+        </section>
+        &copy; 2024 Creado por Grupo
+    </footer>
+</body>
+
+</html>
