aed0daee9c5ea63faf3de1815e77a51b191a8e96
Rc.JosephMark <rc.josephmark@gmail.com>
2024-06-02 15:57:25 -0500

    Se agrego paginas de demostracion de los platos de la carga y una plantilla en blanco de la pagina (con encabezado y pie de pagina)

---
+++ Pag_MarCriollo/src/JavaScript/producto.js
@@ -0,0 +1,31 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const track = document.querySelector('.carousel-track');
+    const slides = Array.from(track.children);
+    const nextButton = document.querySelector('.botonDer');
+    const prevButton = document.querySelector('.botonIzq');
+    const slideWidth = slides[0].getBoundingClientRect().width;
+
+    let currentIndex = 0;
+
+    const moveToSlide = (index) => {
+        track.style.transform = `translateX(-${slideWidth * index}px)`;
+        currentIndex = index;
+    };
+
+    nextButton.addEventListener('click', () => {
+        if (currentIndex === slides.length - 1) {
+            moveToSlide(0);
+        } else {
+            moveToSlide(currentIndex + 1);
+        }
+    });
+
+    prevButton.addEventListener('click', () => {
+        if (currentIndex === 0) {
+            moveToSlide(slides.length - 1);
+        } else {
+            moveToSlide(currentIndex - 1);
+        }
+    });
+});
