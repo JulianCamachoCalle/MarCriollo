e795a27c0aa0027d2cfd68e3abc178e5129fb579
JhordyCC <jhordycontreras08@gmail.com>
2024-06-03 01:47:12 -0500

    triste

--- Pag_MarCriollo/src/JavaScript/contacto.js
+++ Pag_MarCriollo/src/JavaScript/contacto.js
@@ -16,18 +16,26 @@ const typed = new Typed('.typed', {
     contentType: 'html',
 });
 
+
 emailjs.init('2PycRQz0__5oeSeui');
+
+const btn = document.getElementById('button');
 const form = document.getElementById('form');
+
 form.addEventListener('submit', function(event) {
     event.preventDefault(); // Prevenir el comportamiento predeterminado del formulario
-    const btn = document.getElementById('button');
+
     btn.value = 'Enviando...'; // Cambiar el valor del botón a 'Enviando...' mientras se envía el formulario
+
     const serviceID = 'default_service'; // ID del servicio de EmailJS
     const templateID = 'template_g18tx0s'; // ID de la plantilla de EmailJS
+
+    // Enviar el formulario utilizando EmailJS
     emailjs.sendForm(serviceID, templateID, this)
         .then(() => {
-            // Redirigir al usuario a una página de confirmación
-            window.location.href = 'PHP/contacto.php';
+            btn.value = 'Enviar mensaje'; // Cambiar el valor del botón de vuelta a 'Enviar Email' después de enviar el formulario
+            alert('¡Enviado!'); // Mostrar una alerta indicando que el mensaje ha sido enviado correctamente
+            form.reset(); // Restablecer los valores del formulario
         })
         .catch((err) => {
             btn.value = 'Enviar mensaje'; // Cambiar el valor del botón de vuelta a 'Enviar Email' en caso de error
